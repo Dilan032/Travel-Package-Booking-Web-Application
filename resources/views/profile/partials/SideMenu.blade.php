24cf1fad23ddfdb8774ed0ffad8399d0840bcbc9
Dilan kanishka <dilankanishka032@gmail.com>
2024-05-11 18:14:00 +0530

    add icons to profile dashboard

--- resources/views/profile/partials/SideMenu.blade.php
+++ resources/views/profile/partials/SideMenu.blade.php
@@ -3,16 +3,28 @@
     
     <div class="grid gap-3">
         <a class="{{ request()->is('profile/Dashbord') ? 'active' : '' }}" href="{{ route('profile.Dashbord') }}" >
-            <div class="p-2 g-col-6 mouse-hand menu-item"  >Dashboard</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item"  >
+                <img src="{{ asset('image/help-tools/dashboard.png') }}" class="dashbord-icon" alt="dashbord-icon">    
+                Dashboard
+            </div>
         </a>
         <a class="{{ request()->is('profile/Edit') ? 'active' : '' }}" href="{{ route('profile.edit') }}">
-            <div class="p-2 g-col-6 mouse-hand menu-item" >Edit Profile</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item" >
+                <img src="{{ asset('image/help-tools/edit.png') }}" class="dashbord-icon" alt="dashbord-icon">
+                Edit Profile
+            </div>
         </a>
         <a class="{{ request()->is('profile/Edit/password') ? 'active' : '' }}" href="{{ route('profile.profileChangePassword') }}">
-            <div class="p-2 g-col-6 mouse-hand menu-item" >Change Password</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item" >
+                <img src="{{ asset('image/help-tools/password.png') }}" class="dashbord-icon" alt="dashbord-icon">
+                Change Password
+            </div>
         </a>
         <a class="{{ request()->is('profile') ? 'active' : '' }}" href="{{ route('profile.destroy') }}">
-            <div class="p-2 g-col-6 mouse-hand menu-item" >Delete Account</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item" >
+                <img src="{{ asset('image/help-tools/warning.png') }}" class="dashbord-icon" alt="dashbord-icon">
+                Delete Account
+            </div>
         </a>
     </div>
     
@@ -20,10 +32,16 @@
     <h4 class="mt-4">Tour Booking</h4>
     <div class="grid gap-3">
         <a class="{{ request()->is('profile/Booking') ? 'active' : '' }}" href="{{ route('profile.Booking') }}">
-            <div class="p-2 g-col-6 mouse-hand menu-item">My Booking</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item">
+                <img src="{{ asset('image/help-tools/booking.png') }}" class="dashbord-icon" alt="dashbord-icon">
+                My Booking
+            </div>
         </a>
         <a class="{{ request()->is('profile/invoice') ? 'active' : '' }}" href="{{ route('profile.Invoice') }}">
-            <div class="p-2 g-col-6 mouse-hand menu-item">Invoices</div>
+            <div class="p-1 g-col-6 mouse-hand menu-item">
+                <img src="{{ asset('image/help-tools/invoice.png') }}" class="dashbord-icon" alt="dashbord-icon">
+                Invoices
+            </div>
         </a>
     </div>
 
